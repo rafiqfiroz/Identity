47bdaa79a014f13a1a7889f9ceed6af34d699ea1
rafiqfiroz <rafiqahmed.com@gmail.com>
2024-08-29 12:23:17 +0600

    Custom Validation policies for Email and Password

--- Identity/Controllers/AdminController.cs
+++ Identity/Controllers/AdminController.cs
@@ -10,6 +10,9 @@ namespace Identity.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private IPasswordHasher<AppUser> _passwordHasher;
+        private IPasswordValidator<AppUser> passwordValidator;
+        private IUserValidator<AppUser> userValidator;
+
         public AdminController(UserManager<AppUser> userManager,IPasswordHasher<AppUser>passwordHasher)
         {
             _userManager = userManager;
@@ -62,23 +65,66 @@ namespace Identity.Controllers
             }
            
         }
+        //[HttpPost]
+        //public async Task<IActionResult> Update(string id, string email, string password)
+        //{
+        //    AppUser user = await _userManager.FindByIdAsync(id);
+        //    if (user != null)
+        //    {
+        //        if (!string.IsNullOrEmpty(email))
+        //            user.Email = email;
+        //        else
+        //            ModelState.AddModelError("", "Email cannot be empty");
+
+        //        if (!string.IsNullOrEmpty(password))
+        //            user.PasswordHash = _passwordHasher.HashPassword(user, password);
+        //        else
+        //            ModelState.AddModelError("", "Password cannot be empty");
+
+        //        if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+        //        {
+        //            IdentityResult result = await _userManager.UpdateAsync(user);
+        //            if (result.Succeeded)
+        //                return RedirectToAction("Index");
+        //            else
+        //                Errors(result);
+        //        }
+        //    }
+        //    else
+        //        ModelState.AddModelError("", "User Not Found");
+        //    return View(user);
+        //}
         [HttpPost]
         public async Task<IActionResult> Update(string id, string email, string password)
         {
             AppUser user = await _userManager.FindByIdAsync(id);
             if (user != null)
             {
+                IdentityResult validEmail = null;
                 if (!string.IsNullOrEmpty(email))
-                    user.Email = email;
+                {
+                    validEmail = await userValidator.ValidateAsync(_userManager, user);
+                    if (validEmail.Succeeded)
+                        user.Email = email;
+                    else
+                        Errors(validEmail);
+                }
                 else
                     ModelState.AddModelError("", "Email cannot be empty");
 
+                IdentityResult validPass = null;
                 if (!string.IsNullOrEmpty(password))
-                    user.PasswordHash = _passwordHasher.HashPassword(user, password);
+                {
+                    validPass = await passwordValidator.ValidateAsync(_userManager, user, password);
+                    if (validPass.Succeeded)
+                        user.PasswordHash = _passwordHasher.HashPassword(user, password);
+                    else
+                        Errors(validPass);
+                }
                 else
                     ModelState.AddModelError("", "Password cannot be empty");
 
-                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                if (validEmail != null && validPass != null && validEmail.Succeeded && validPass.Succeeded)
                 {
                     IdentityResult result = await _userManager.UpdateAsync(user);
                     if (result.Succeeded)
@@ -89,6 +135,7 @@ namespace Identity.Controllers
             }
             else
                 ModelState.AddModelError("", "User Not Found");
+
             return View(user);
         }
 
