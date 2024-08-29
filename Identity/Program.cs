fa60a2808e509a64a6dc36c5f0e30336665303b4
rafiqfiroz <rafiqahmed.com@gmail.com>
2024-08-29 12:11:30 +0600

    Custom User policy

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,4 +1,5 @@
 using Identity.Data;
+using Identity.IdentityPolicy;
 using Identity.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]));
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+builder.Services.AddTransient<IUserValidator<AppUser>, CustomUsernameEmailPolicy>();
 builder.Services.Configure<IdentityOptions>(opts =>
 {
     opts.User.RequireUniqueEmail = true;
